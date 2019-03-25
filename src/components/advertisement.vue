9287fa1ce747576908dea30731690e1c8efcd065
FarmanKKK <13950169615@163.com>
2019-03-25 16:34:51 +0800

    首页构建中

---
+++ src/components/advertisement.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="advertisement-card">
+		<div class="advertisement-content">
+		
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style lang="sass" scoped>
+	.advertisement-card
+		margin-top: 30px
+		.advertisement-content
+			width: 690px
+			height: 388px
+			background-color: #ffffff
+			margin: 0 auto
+</style>
--- src/pages/index/entry.vue
+++ src/pages/index/entry.vue
@@ -6,7 +6,91 @@
       title-type="icon"
       @wxcTabBarCurrentTabSelected="wxcTabBarCurrentTabSelected">
       <!-- 第一个页面内容-->
-      <div class="item-container"><text>首页</text></div>
+      <div class="item-container">
+        <!-- 首页头部区域 -->
+        <div class="home-head mb17">
+          <div class="home-head-sec1">
+            <div class="home-head-icon">
+              <image :src="headIcon" style="width:180px;height:180px"></image>
+              <text class="name">奥利</text>
+            </div>
+            <div class="home-head-name">
+              <div>
+                <wxc-cell 
+                  title="奥利，早上好"
+                  desc="很荣幸为您服务"
+                  :has-arrow="true"
+                  @wxcCellClicked="wxcCellClicked"
+                  :cell-style="cellStyle"
+                  :has-top-border="false"
+                  :has-bottom-border="false"></wxc-cell>
+              </div>
+              <div class="head-icon-group">
+                <div class="head-contact head-icon">
+                  <image :src="iconGroup.iconPhone" style="width:56px;height:56px"></image>
+                  <text class="info-text">联系我</text>
+                </div>
+                <div class="head-service-phone head-icon">
+                  <image :src="iconGroup.iconService" style="width:56px;height:56px"></image>
+                  <text class="info-text">400热线</text>
+                </div>
+                <div class="head-service-online head-icon">
+                  <image :src="iconGroup.iconMessage" style="width:56px;height:56px"></image>
+                  <text class="info-text">线上客服</text>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="home-head-sec1_foot">
+            <wxc-cell 
+              :has-arrow="true"
+              :cell-style="footStyle"
+              @wxcCellClicked="wxcCellClicked"
+              :has-top-border="false">
+              <text slot="title" style="color: #5387D9">已收到您诊所的预约，我正火速赶来</text>
+            </wxc-cell>
+          </div>
+        </div>
+        <!-- 首页三块功能区 -->
+        <div class="home-service mb17">
+          <div class="home-service_content">
+            <image :src="iconGroup2.iconWatch" style="width:102px;height:94px"></image>
+            <text class="info-text">预约检验</text>
+          </div>
+          <div class="home-service_content">
+            <image :src="iconGroup2.iconReport" style="width:102px;height:94px"></image>
+            <text class="info-text">报告解读</text>
+          </div>
+          <div class="home-service_content">
+            <image :src="iconGroup2.iconPay" style="width:102px;height:94px"></image>
+            <text class="info-text">检验费支付</text>
+          </div>
+        </div>
+        <!-- 首页消息通知区 -->
+        <div class="home-message">
+          <wxc-cell 
+              :has-arrow="true"
+              @wxcCellClicked="wxcCellClicked"
+              :has-bottom-border="false"
+              :has-top-border="false">
+              <image :src="iconMsg" style="width:90px;height:80px" slot="label"></image>
+              <div class="msg-list" slot="title">
+                <div class="msg-item" v-for="(item,index) in msgList" :key="index">
+                  <text class="msg-content">{{item.content}}</text><text class="msg-time">{{item.time}}</text>
+                </div>
+              </div>
+            </wxc-cell>
+        </div>
+        <!-- 首页广告区 -->
+        <div class="home-advertisement">
+          <adver-card>
+
+          </adver-card>
+          <adver-card>
+            
+          </adver-card>
+        </div>
+      </div>
 
       <!-- 第二个页面内容-->
       <div class="item-container"><text>特别推荐</text></div>
@@ -24,20 +108,40 @@
 </template>
 
 <script>
-import { WxcTabBar, Utils } from 'weex-ui';
+import { WxcCell, WxcTabBar, Utils } from 'weex-ui';
 import { createLink, getImg } from '../../utils/index'
+import AdverCard from '../../components/advertisement'
 const navigator = weex.requireModule('navigator')
 
 export default {
   name: 'App',
   components: {
-    WxcTabBar
+    WxcTabBar,
+    WxcCell,
+    AdverCard
   },
   data () {
     return {
       testIcon: getImg('icon_boss.png'),
-      contentStyle: {
-
+      headIcon: getImg('user_icon.png'),
+      iconMsg: getImg('show_msg.png'),
+      iconGroup: {
+        iconPhone: getImg('phone.png'),
+        iconService: getImg('service.png'),
+        iconMessage: getImg('message.png'),
+      },
+      iconGroup2: {
+        iconWatch: getImg('watch.png'),
+        iconReport: getImg('report.png'),
+        iconPay: getImg('pay.png')
+      },
+      cellStyle: {
+        height: '100px'
+      },
+      footStyle: {
+        backgroundColor: '#EFF5FF',
+        height: '64px',
+        width: '664px'
       },
       tabTitles: [
         {
@@ -77,6 +181,13 @@ export default {
         textPaddingLeft: 10,
         textPaddingRight: 10
       },
+      msgList: [{
+        content: '您的医师责任险购买成功',
+        time: '10分钟前'
+      },{
+        content: '您的检测预约成功',
+        time: '2天前'
+      }],
     }
   },
   methods: {
@@ -92,6 +203,9 @@ export default {
     wxcTabBarCurrentTabSelected (e) {
       const index = e.page;
       console.log(index);
+    },
+    wxcCellClicked(){
+      console.log('点击姓名')
     }
   }
 }
@@ -99,10 +213,72 @@ export default {
 
 <style lang='sass' scoped>
   $pageColor: #ffffff
+  $footColor: #5387D9
   #home
+    background-color: #F8F8F8
     .item-container 
       width: 750px
-      background-color: $pageColor
-      align-items: center
-      justify-content: center
+      .mb17
+        margin-bottom: 17px
+      .mb30
+        margin-bottom: 30px
+      .mt30
+        margin-top: 30px
+      .home-head
+        height: 441px
+        background-color: $pageColor
+      .home-head-sec1_foot
+        margin: 42px auto
+        .cell-content
+          font-size: 20px
+      .home-head-sec1
+        width: 100%
+        display: flex
+        flex-direction: row
+        justify-content: space-around
+        padding-top: 50px
+        .home-head-icon
+          flex: 1
+          align-items: center
+          .name
+            font-size: 46px
+        .home-head-name
+          flex: 2
+        .head-icon-group
+          margin-top: 42px
+          display: flex
+          flex-direction: row
+          justify-content: space-around
+          .info-text
+            color: #A3A3A3
+          div
+            align-items: center
+      .home-service
+        height: 266px
+        background-color: $pageColor
+        display: flex
+        flex-direction: row
+        justify-content: space-around
+        padding: 35px 10px 0
+        .home-service_content
+          padding-top: 32px
+          align-items: center
+          width: 215px
+          height: 195px
+          border-radius: 6px
+          border: 1px solid rgba(83,135,217,0.2)
+          .info-text
+            color: #5387D9
+      .home-message
+        height: 133px
+        background-color: $pageColor
+        .msg-item
+          padding-left: 40px
+          display: flex
+          flex-direction: row
+          font-size: 28px
+          .msg-content
+            margin-right: 19px
+          .msg-time
+            color: #999999
 </style>
